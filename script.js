059aa6731f57dddb732a6746b88d8476e0868b8b
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-28 16:09:25 -0400

    Create all the constant to have access by document.querySelector to the inputs, the button and the empty ul with id bookList on script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const bookList = document.getElementById('bookList');
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const addButton = document.querySelector('#bookForm button');
