1584002f219d75ccd93e23b96e5aceaa40181f8d
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-28 16:15:56 -0400

    Create the empty variable called books as a array and the function loadBooksFromLocalStorage to charge the storage books and show them in the list

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const bookList = document.getElementById('bookList');
 const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
 const addButton = document.querySelector('#bookForm button');
+
+let books = [];
+
+const loadBooksFromLocalStorage = () => {
+  const storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+    displayBooks();
+  }
+};
